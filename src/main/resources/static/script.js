ff864c8f26ff8a1bc9ced0bce4da2f12e3d57bbe
LucasFlorencio-Siqueira <lhfs2@cesar.school>
2026-03-28 09:25:42 -0300

    Subindo logica de validação dos formularios no js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,17 +1,27 @@
 document.getElementById('impactForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
 
-
     const loading = document.getElementById('loadingState');
     const result = document.getElementById('resultState');
     const valorCO2e = document.getElementById('valorCO2e');
-
     const transacoes = document.getElementById('numeroTransacoes').value;
     const tipo = document.getElementById('tipoPagamento').value;
-    
+
+    if (transacoes == "") {
+        alert("O número de transações precisa ser informado");
+        return; 
+    }
+    if (transacoes <= 0) {
+        alert("o numero de transações precisa ser maior que 0");
+        return;
+    }
+    if (tipo == "") {
+        alert("Selecione um meio de pagamento");
+        return;
+    }
+
     if (loading) loading.classList.remove('hidden');
         if (result) result.classList.add('hidden');
-
     const payload = {
         numeroTransacoes: parseInt(transacoes),
         tipoPagamento: tipo
@@ -21,22 +31,21 @@ document.getElementById('impactForm').addEventListener('submit', function(event)
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
-    })
+    }
+)
     .then(response => response.text())
     .then(data => {
-
-        if (loading) loading.classList.add('hidden');
+        if (data == "erro") {
+            alert("erro no servidor!");
+        } else {
+            if (loading) loading.classList.add('hidden');
             if (result) {
                 result.classList.remove('hidden');
                 valorCO2e.innerText = data; 
+            }
         }
-
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-            if (loading) loading.classList.add('hidden');
-                alert("Erro na conexão.");
-    });
-});
-
+    }
+)
+}
+);
 
